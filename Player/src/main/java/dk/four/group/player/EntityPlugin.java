7a9403af7315666092c336a75d50dea8a6c4a373
balachanthiran <varunsasy@hotmail.com>
2016-04-15 17:04:41 +0200

    The game works and the player is loaded

--- Player/src/main/java/dk/four/group/player/EntityPlugin.java
+++ Player/src/main/java/dk/four/group/player/EntityPlugin.java
@@ -1,10 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dk.four.group.player;
-
 
 import dk.four.group.common.data.Entity;
 import static dk.four.group.common.data.EntityType.PLAYER;
@@ -17,7 +11,7 @@ import org.openide.util.lookup.ServiceProvider;
  *
  * @author Varun
  */
-@ServiceProvider (service = IGamePluginService.class)
+@ServiceProvider(service = IGamePluginService.class)
 public class EntityPlugin implements IGamePluginService {
 
     private Map<String, Entity> world;
@@ -49,11 +43,11 @@ public class EntityPlugin implements IGamePluginService {
 
         playerShip.setRadians(3.1415f / 2);
         playerShip.setRotationSpeed(5);
-        
+
         playerShip.setLife(1);
-        
+
         playerShip.setRadius(4);
-        
+
         return playerShip;
     }
 
