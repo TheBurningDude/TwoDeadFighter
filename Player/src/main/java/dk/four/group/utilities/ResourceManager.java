4d2b4d0ea96dc04e29c3c90b648324487c23f395
TheBurningDude <nasibo2005@hotmail.com>
2016-04-16 14:17:45 +0200

    Projekt kører for alle

---
+++ Player/src/main/java/dk/four/group/utilities/ResourceManager.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.four.group.utilities;
+
+/**
+ *
+ * @author nasib
+ */
+public class ResourceManager {
+    
+    public static void load(){
+        
+    }
+    
+    public static Boolean isLoaded(){
+        return false;
+    }
+    
+    public static void dispose(){
+        
+    }
+}
