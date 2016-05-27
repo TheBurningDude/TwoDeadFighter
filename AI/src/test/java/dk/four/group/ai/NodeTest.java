285d795f22d163720197b8f29d1fc9613cfe3079
SidonKK <sidonkk@gmail.com>
2016-05-27 10:29:43 +0200

    test

---
+++ AI/src/test/java/dk/four/group/ai/NodeTest.java
@@ -0,0 +1,141 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.four.group.ai;
+
+import dk.four.group.common.data.NodeType;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Sidon K. K
+ */
+public class NodeTest {
+    
+    public NodeTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    /**
+     * Test of getX method, of class Node.
+     */
+    @Test
+    public void testGetX() {
+        System.out.println("getX");
+        Node instance = new Node(0, 0);
+        float expResult = 0;
+        float result = instance.getX();
+        assertEquals(expResult, result, 0.0);
+    }
+
+    /**
+     * Test of getY method, of class Node.
+     */
+    @Test
+    public void testGetY() {
+        System.out.println("getY");
+        Node instance = new Node(0, 0);
+        float expResult = 0;
+        float result = instance.getY();
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+       
+    }
+
+    /**
+     * Test of getF method, of class Node.
+     */
+    @Test
+    public void testGetF() {
+        System.out.println("getF");
+        Node instance = new Node(0, 0);
+        float expResult = 0;
+        float result = instance.getF();
+        assertEquals(expResult, result, 0.0);
+
+    }
+
+    /**
+     * Test of getG method, of class Node.
+     */
+    @Test
+    public void testGetG() {
+        System.out.println("getG");
+        Node instance = new Node(0, 0);
+        float expResult = 0;
+        float result = instance.getG();
+        assertEquals(expResult, result, 0.0);
+       
+    }
+
+    /**
+     * Test of getH method, of class Node.
+     */
+    @Test
+    public void testGetH() {
+        System.out.println("getH");
+        Node instance = new Node(0, 0);
+        float expResult = 0;
+        float result = instance.getH();
+        assertEquals(expResult, result, 0.0);
+
+    }
+
+    /**
+     * Test of setF method, of class Node.
+     */
+    @Test
+    public void testSetF() {
+        System.out.println("setF");
+        float F = 10;
+        Node instance = new Node(0, 0);
+        instance.setF(F);
+        assertEquals(F,instance.getF(), 0.0);
+    }
+
+    /**
+     * Test of setG method, of class Node.
+     */
+    @Test
+    public void testSetG() {
+        System.out.println("setG");
+        float G = 15;
+        Node instance = new Node(0, 0);
+        instance.setG(G);
+        assertEquals(G,instance.getG(), 0.0);
+    }
+
+    /**
+     * Test of setH method, of class Node.
+     */
+    @Test
+    public void testSetH() {
+        System.out.println("setH");
+        float H = 50;
+        Node instance = new Node(0, 0);
+        instance.setH(H);
+        assertEquals(H,instance.getH(), 0.0);
+    }
+    
+}
---
+++ AI/src/test/java/dk/four/group/ai/PathFinderTest.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.four.group.ai;
+
+import dk.four.group.common.data.Entity;
+import dk.four.group.common.data.GameData;
+import dk.four.group.common.data.NodeType;
+import dk.four.group.common.services.IGamePluginService;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.netbeans.junit.MockServices;
+/**
+ *
+ * @author Sidon K. K
+ */
+public class PathFinderTest {
+    
+    public PathFinderTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+        
+    }
+    
+    
+    public static Entity createDummyEntity(){
+        Entity e = new Entity();
+        e.setEntityPosition(0, 0);
+        return e;
+            
+        
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of findPath method, of class PathFinder.
+     */
+    @Test
+    public void testFindPath() {
+         System.out.println("findPath");
+        Entity s = createDummyEntity();
+        Entity e = createDummyEntity();
+        PathFinder instance = new PathFinder(320, 320, 64,64);
+        instance.SetGridNode((int) s.getEntityPosition().getX(), (int) s.getEntityPosition().getY(), NodeType.START);
+        instance.SetGridNode((int) e.getEntityPosition().getX(), (int) e.getEntityPosition().getY(), NodeType.END);
+        int expResult = 1;
+        int result = instance.findPath();
+        assertEquals(expResult, result);
+    }
+    
+    
+    public static final class MapMock implements IGamePluginService{
+
+        private Map<String, Entity> world;
+        
+        
+        @Override
+        public void start(GameData gameData, Map<String, Entity> world) {
+            this.world = (Map<String, Entity>) world.values();
+        }
+
+        @Override
+        public void stop(GameData gamedata) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+        
+    }
+    
+    
+}
